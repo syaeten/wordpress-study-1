9c02d5c3f3ab1d49a2796921a5ef1820183fd5fb
syaeten <sya.eten@canbright.co.jp>
2023-04-27 16:43:29 +0800

    setting images

--- sya_study/index.php
+++ sya_study/index.php
@@ -2,6 +2,23 @@
 
 <main class="page-front">
 
+  <img class='bg-img' src="<?php echo get_template_directory_uri() . '/image/top_main_img.png' ?>" alt="top_main_img">
+
+  <div class='service-imgs'>
+    <img class='service-img-main' src="<?php echo get_template_directory_uri() . '/image/service_saas.svg' ?>"
+      alt="service_saas">
+    <img class='service-img service_seizou' id='service_seizou'
+      src="<?php echo get_template_directory_uri() . '/image/service_seizou.png' ?>" alt="service_seizou">
+    <img class='service-img' id='service_zaiko'
+      src="<?php echo get_template_directory_uri() . '/image/service_zaiko.png' ?>" alt="service_zaiko">
+    <img class='service-img service_zyuhattyu' id='service_zyuhattyu'
+      src="<?php echo get_template_directory_uri() . '/image/service_zyuhattyu.png' ?>" alt="service_zyuhattyu">
+    <img class='service-img service_kiroku' id='service_kiroku'
+      src="<?php echo get_template_directory_uri() . '/image/service_kiroku.png' ?>" alt="service_kiroku">
+  </div>
+
+
+
 </main>
 <?php get_footer(); ?>
 </body>
