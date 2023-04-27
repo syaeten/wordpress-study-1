519ad79b1a367ed7e4188c1a4878bf64f1971957
syaeten <sya.eten@canbright.co.jp>
2023-04-27 22:54:21 +0800

    added some css

--- sya_study/index.php
+++ sya_study/index.php
@@ -7,16 +7,47 @@
   <div class='service-imgs'>
     <img class='service-img-main' src="<?php echo get_template_directory_uri() . '/image/service_saas.svg' ?>"
       alt="service_saas">
-    <img class='service-img service_seizou' id='service_seizou'
+    <img class='service-img' id='service_seizou'
       src="<?php echo get_template_directory_uri() . '/image/service_seizou.png' ?>" alt="service_seizou">
     <img class='service-img' id='service_zaiko'
       src="<?php echo get_template_directory_uri() . '/image/service_zaiko.png' ?>" alt="service_zaiko">
-    <img class='service-img service_zyuhattyu' id='service_zyuhattyu'
+    <img class='service-img' id='service_zyuhattyu'
       src="<?php echo get_template_directory_uri() . '/image/service_zyuhattyu.png' ?>" alt="service_zyuhattyu">
-    <img class='service-img service_kiroku' id='service_kiroku'
+    <img class='service-img' id='service_kiroku'
       src="<?php echo get_template_directory_uri() . '/image/service_kiroku.png' ?>" alt="service_kiroku">
   </div>
 
+  <div class='flow-head'>
+    <img src="<?php echo get_template_directory_uri() . '/image/flow_text.svg' ?>" alt="flow_text">
+    <h2>導入までの流れ</h2>
+  </div>
+  <div class='flow-box'>
+    <div class='flow-item'>
+      <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title1.svg' ?>"
+        alt="flow_title1">
+      <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure1.png' ?>"
+        alt="flow_figure1">
+    </div>
+    <div class='flow-item'>
+      <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title2.svg' ?>"
+        alt="flow_title2">
+      <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure2.png' ?>"
+        alt="flow_figure2">
+    </div>
+    <div class='flow-item'>
+      <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title3.svg' ?>"
+        alt="flow_title3">
+      <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure3.png' ?>"
+        alt="flow_figure3">
+    </div>
+    <div class='flow-item'>
+      <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title4.svg' ?>"
+        alt="flow_title4">
+      <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure4.png' ?>"
+        alt="flow_figure4">
+    </div>
+
+  </div>
 
 
 </main>
