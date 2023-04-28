9c9d21237e6ab7df33eab8a457f10a080235f590
syaeten <sya.eten@canbright.co.jp>
2023-04-28 17:16:24 +0800

    fixed some css

--- sya_study/index.php
+++ sya_study/index.php
@@ -11,23 +11,18 @@
       <img src="<?php echo get_template_directory_uri() . '/image/service_text.svg' ?>" alt="service_text">
       <h2>サービス一覧</h2>
     </div>
-    <img class='service-img-main' src="<?php echo get_template_directory_uri() . '/image/service_saas.svg' ?>"
-      alt="service_saas">
+    <img class='service-img-main' src="<?php echo get_template_directory_uri() . '/image/service_saas.svg' ?>" alt="service_saas">
     <a href="">
-      <img class='service-img' id='service_seizou'
-        src="<?php echo get_template_directory_uri() . '/image/service_seizou.png' ?>" alt="service_seizou">
+      <img class='service-img' id='service_seizou' src="<?php echo get_template_directory_uri() . '/image/service_seizou.png' ?>" alt="service_seizou">
     </a>
     <a href="">
-      <img class='service-img' id='service_zaiko'
-        src="<?php echo get_template_directory_uri() . '/image/service_zaiko.png' ?>" alt="service_zaiko">
+      <img class='service-img' id='service_zaiko' src="<?php echo get_template_directory_uri() . '/image/service_zaiko.png' ?>" alt="service_zaiko">
     </a>
     <a href="">
-      <img class='service-img' id='service_zyuhattyu'
-        src="<?php echo get_template_directory_uri() . '/image/service_zyuhattyu.png' ?>" alt="service_zyuhattyu">
+      <img class='service-img' id='service_zyuhattyu' src="<?php echo get_template_directory_uri() . '/image/service_zyuhattyu.png' ?>" alt="service_zyuhattyu">
       <a href="">
       </a>
-      <img class='service-img' id='service_kiroku'
-        src="<?php echo get_template_directory_uri() . '/image/service_kiroku.png' ?>" alt="service_kiroku">
+      <img class='service-img' id='service_kiroku' src="<?php echo get_template_directory_uri() . '/image/service_kiroku.png' ?>" alt="service_kiroku">
     </a>
   </div>
 
@@ -40,28 +35,24 @@
     </div>
     <div class='flex-center-box flow-page-box'>
       <div class='flow-page-item'>
-        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title1.svg' ?>"
-          alt="flow_title1">
-        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure1.png' ?>"
-          alt="flow_figure1">
+        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title1.svg' ?>" alt="flow_title1">
+        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure1.png' ?>" alt="flow_figure1">
+        <p>デモを通して機能を紹介させていただきます。一部の機能からでも導入いただけますのでご興味のある機能に絞ったご提案も可能です。</p>
       </div>
       <div class='flow-page-item'>
-        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title2.svg' ?>"
-          alt="flow_title2">
-        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure2.png' ?>"
-          alt="flow_figure2">
+        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title2.svg' ?>" alt="flow_title2">
+        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure2.png' ?>" alt="flow_figure2">
+        <p>利用する機能やライセンス数を決定いただき発注いただきます。 クラウドサインを利用した電子契約でお願いしております。</p>
       </div>
       <div class='flow-page-item'>
-        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title3.svg' ?>"
-          alt="flow_title3">
-        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure3.png' ?>"
-          alt="flow_figure3">
+        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title3.svg' ?>" alt="flow_title3">
+        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure3.png' ?>" alt="flow_figure3">
+        <p>導入のスケジュールや進め方について打ち合わせします。</p>
       </div>
       <div class='flow-page-item'>
-        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title4.svg' ?>"
-          alt="flow_title4">
-        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure4.png' ?>"
-          alt="flow_figure4">
+        <img class='flow-title' src="<?php echo get_template_directory_uri() . '/image/flow_title4.svg' ?>" alt="flow_title4">
+        <img class='flow-figure' src="<?php echo get_template_directory_uri() . '/image/flow_figure4.png' ?>" alt="flow_figure4">
+        <p>導入支援サービスを申込みいただいたお客様に現地またはオンラインでの支援をさせていただきます。</p>
       </div>
     </div>
   </div>
