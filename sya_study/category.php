0e89f26d7227a2b06bceed31078dead1b354b1a1
syaeten <sya.eten@canbright.co.jp>
2023-06-02 14:19:41 +0900

    category page

--- sya_study/category.php
+++ sya_study/category.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
+<h1>カテゴリー：『<?php single_cat_title(); ?>』の記事一覧</h1>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <a href="<?php the_permalink() ?>"></a>
-    <?php endwhile; ?>
+    <a href="<?php the_permalink() ?>"></a>
+  <?php endwhile; ?>
 <?php endif; ?>
 
 </main>
